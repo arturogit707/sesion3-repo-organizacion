a84bc58da0b4ca570b9e29fa117d7f6d9ca951b6
arturogit707 <82395768+arturogit707@users.noreply.github.com>
2021-05-02 19:33:30 +0200

    Create calculator.php

---
+++ calculator.php
@@ -0,0 +1,32 @@
+<?php
+class Calculator
+{
+ 
+    public function add($a, $b): float
+    {
+        return $a + $b;
+    }
+
+    public function multiply($a, $b): float
+    {
+        return $a * $b;
+    }
+
+    public function division($a, $b): float
+    {
+        return $a / $b;
+    }
+ 
+}
+© 2021 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
